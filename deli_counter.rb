ce1d367a4a4e7105d95607b6e744e6ae8456261a
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 19:46:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(name)
   if katz_deli.length === 0 
     puts "The line is currently empty."
   else 
-    katz_deli.each_with_index do |name, index| katz_deli << "#{index+1}. #{name}"
+    name.each_with_index do |name, index| katz_deli << "#{index+1}. #{name}"
   end
     puts "The line is currently: #{katz_deli}"
   end
